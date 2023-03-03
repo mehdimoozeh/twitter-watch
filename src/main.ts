d436952833838a617a13ff2609f6b97707483c24
Mehdi <me.moozeh@gmail.com>
2023-03-03 11:35:55 +0300

    chore: config backend

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const configService = app.get(ConfigService);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      skipMissingProperties: true,
+      enableDebugMessages: configService.get('NODE_ENV') !== 'production',
+    }),
+  );
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Twitter Watch')
+    .setDescription('')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // Start server
+  const PORT = +configService.get('PORT');
+  const logger = new Logger();
+  await app.listen(PORT, () => {
+    logger.log(`Listening on port ${PORT}`);
+  });
 }
 bootstrap();

27bde9555cb8e25d5432255e80b37d6295c1cac8
Mehdi <me.moozeh@gmail.com>
2023-03-10 20:11:50 +0300

    feat: add mongo

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './healthController';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TwitterWatchModule } from './twitter-watch/twitter-watch.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TwitterWatchModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGO_URI'),
+        useNewUrlParser: true,
+      }),
+      inject: [ConfigService],
+    }),
+    TwitterWatchModule,
+  ],
   controllers: [HealthController],
   providers: [],
 })
